6fc5aa25ec67def9c52dde273bcb04b7145f2381
John <baltz@usc.edu>
2018-04-15 13:10:56 -0700

    Uploaded Audio Player

---
+++ AudioPlayer.py
@@ -0,0 +1,38 @@
+#coding=utf-8
+
+
+import pyaudio
+import wave
+
+
+#define stream chunk
+chunk = 1024
+
+#open a wav format music
+f = wave.open("first.wav", "rb")
+print(f.getnframes())
+print("Frame Rate: ", f.getframerate())
+
+
+
+#instantiate PyAudio
+p = pyaudio.PyAudio()
+#open stream
+stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
+                channels = f.getnchannels(),
+                rate = f.getframerate(),
+                output = True)
+#read data
+data = f.readframes(chunk)
+
+#play stream
+while data:
+    stream.write(data)
+    data = f.readframes(chunk)
+
+#stop stream
+stream.stop_stream()
+stream.close()
+
+#close PyAudio
+p.terminate()
