d861bd996c765dc1b22aee66df49c68bd0edf40c
Taufeq  Razakh <>
2018-04-22 16:47:30 -0700

    testing open cv histogram =features here

--- qAndDbHist.py
+++ qAndDbHist.py
@@ -8,14 +8,19 @@ import cv2
 DatabaseAd = "/Users/taufeqrazakh/Documents/school/CSCI 576/Project_CSCI_567/databse_videos/"
 QueryAd = "/Users/taufeqrazakh/Documents/school/CSCI 576/Project_CSCI_567/query/first/"
 
-for f in os.listdir(DatabaseAd):
-    folder_name = DatabaseAd+f.title()
-    print(folder_name)
-    if os.path.isdir(folder_name):
-        for pngFiles in os.listdir(folder_name):
-            if '.png' in pngFiles:
-                image = cv2.imshow('showoff',pngFiles)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
+img = cv2.imread('/Users/taufeqrazakh/Documents/school/CSCI 576/Project_CSCI_567/query/first/first001.png')
+cv2.imshow('whatever',img)
+# cv2.calcHist([img], [0], None, [256], [0,256])
+# plt.hist(img.ravel(), 256, [0,256]);plt.show()
+color = ('b', 'g', 'r')
+for i,col in enumerate(color):
+    histr = cv2.calcHist([img], [i], None, [256], [0,256])
+    plt.plot(histr, color = col)
+    plt.xlim([0,256])
+    print (i)
+    print(histr)
+plt.show()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 
