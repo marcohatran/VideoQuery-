e74ed4a54b311027003cf27d9a00b144c3892579
Taufeq  Razakh <>
2018-04-30 01:44:30 -0700

    Get a fantastic looking correlation for rgb values

--- Comparator.py
+++ Comparator.py
@@ -3,6 +3,7 @@ import cv2
 import csv
 import os
 import scipy.spatial.distance
+import scipy.stats
 
 
 class ExtractAllMetrics:
@@ -23,28 +24,6 @@ class ExtractAllMetrics:
     D_G_Array = np.empty((600,256,1))
     D_R_Array = np.empty((600,256,1))
 
-    # Q_R = np.empty((256,1))
-    # Q_G = np.empty((256,1))
-    # Q_B = np.empty((256,1))
-    #
-    # D_R = np.empty((256,1))
-    # D_G = np.empty((256,1))
-    # D_B = np.empty((256,1))
-
-    for i in range (0,600):
-        D_B_Array[i] = ref_array
-        D_G_Array[i] = ref_array
-        D_R_Array[i] = ref_array
-        D_B = ref_array
-        D_G = ref_array
-        D_R = ref_array
-        if i<150:
-            Q_B_Array[i] = ref_array
-            Q_G_Array[i] = ref_array
-            Q_R_Array[i] = ref_array
-            Q_B = ref_array
-            Q_G = ref_array
-            Q_R = ref_array
     # print(ref_array, np.count_nonzero(ref_array))
     # print(Q_B_Array[0], np.size(Q_B_Array[0]),np.count_nonzero(Q_B_Array[0]))
     Image_Correlations = ["/Users/taufeqrazakh/Documents/school/CSCI 576/Project_CSCI_567/databse_videos/flowers/",
@@ -116,20 +95,6 @@ class ExtractAllMetrics:
                             bin = 0
         # print('from datababse B',self.D_B_Array[0])
 
-
-    def transformations(self, index, shift):
-        for j in range(0, 256):
-            self.Q_B[j] = self.Q_B_Array[index][j][0]
-            print(self.Q_B_Array[index][j][0])
-            self.Q_G[j] = self.Q_G_Array[index][j][0]
-            self.Q_R[j] = self.Q_R_Array[index][j][0]
-            self.D_B[j] = self.D_B_Array[index + shift][j][0]
-            self.D_G[j] = self.D_G_Array[index + shift][j][0]
-            self.D_R[j] = self.D_R_Array[index + shift][j][0]
-        print('from transform Q',self.Q_B,'\n','input has to be same as query B', self.Q_B_Array)
-        # print(self.D_B, self.D_B_Array)
-
-
     def compare_hist(self):
         # print(np.shape(Q_B_Array))
         for (line, address) in enumerate(self.Image_Correlations):
@@ -138,13 +103,13 @@ class ExtractAllMetrics:
             for offset in range (0, 450, 20):
                 cumulative_correlation = 0.000
                 for i in range (0,150):
-                    self.transformations(i, offset)
+                    # self.transformations(i, offset)
                     # print(self.Q_B, self.Q_B_Array)
                     # print(self.D_B, self.D_B_Array)
-                    c1 = cv2.compareHist(self.Q_B, self.D_B, cv2.HISTCMP_CORREL)
-                    c2 = cv2.compareHist(self.Q_G, self.D_G, cv2.HISTCMP_CORREL)
-                    c3 = cv2.compareHist(self.Q_R, self.D_R, cv2.HISTCMP_CORREL)
-                    c = float((c1+c2+c3)/3)
+                    c1 = scipy.stats.pearsonr(np.ndarray.flatten(self.Q_B_Array[i]), np.ndarray.flatten(self.D_B_Array[i+offset]))
+                    c2 = scipy.stats.pearsonr(np.ndarray.flatten(self.Q_G_Array[i]), np.ndarray.flatten(self.D_G_Array[i+offset]))
+                    c3 = scipy.stats.pearsonr(np.ndarray.flatten(self.Q_R_Array[i]), np.ndarray.flatten(self.D_R_Array[i+offset]))
+                    c = float((c1[0]+c2[0]+c3[0])/3)
                     cumulative_correlation += c
                     # print(c)
                 cumulative_correlation = cumulative_correlation/150
